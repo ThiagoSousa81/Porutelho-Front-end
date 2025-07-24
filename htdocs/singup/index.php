1afebd04a895712c984e8b653c0781a0fbacade1
Thiago Sousa <thiagosousa81@gmail.com>
2025-07-24 09:56:03 -0300

    § cadastro §

--- htdocs/singup/index.php
+++ htdocs/singup/index.php
@@ -25,8 +25,6 @@
       padding: 30px;
       border-radius: 0.5rem;
       width: 100%;
-      margin-top: 100vh;
-      margin-bottom: 100vh;
       max-width: 360px;
       box-shadow: 0 0 10px rgba(0,0,0,0.5);
     }
@@ -54,18 +52,14 @@
 </head>
 <body>
   <main class="login-container">
-    <h1 class="login-title">Portuelho</h1>
+    <h1 class="login-title text-center">Portuelho</h1>
 
     <form>
       <div class="mb-3 position-relative">
-        <label for="email" class="form-label">Insira o seu Email</label>
+        <label for="email" class="form-label">Email</label>
         <div class="input-group">
           <span class="input-group-text" id="email-addon">
-            <!-- Bootstrap person-circle icon SVG -->
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" aria-hidden="true">
-              <path d="M13.468 12.37C12.758 11.226 11.52 10.5 10 10.5c-1.52 0-2.758.726-3.468 1.87A6.987 6.987 0 0 1 2 8a7 7 0 1 1 11.468 4.37z"/>
-              <path fill-rule="evenodd" d="M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
-            </svg>
+            ✉️
           </span>
           <input
             type="email"
@@ -77,18 +71,35 @@
           />
         </div>
       </div>
+      
+      <div class="mb-3 position-relative">
+        <label for="name" class="form-label">Nome</label>
+        <div class="input-group">
+          <span class="input-group-text" id="name-addon">
+          	😊
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            id="name"
+            placeholder=""
+            aria-describedby="name-addon"
+            required
+          />
+        </div>
+      </div>
 
       <div class="mb-3 position-relative">
-        <label for="username" class="form-label">Nome de usuário</label>
+        <label for="username" class="form-label">Usuário</label>
         <div class="input-group">
           <span class="input-group-text" id="username-addon">
-           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M463 448.2C440.9 409.8 399.4 384 352 384L288 384C240.6 384 199.1 409.8 177 448.2C212.2 487.4 263.2 512 320 512C376.8 512 427.8 487.3 463 448.2zM64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576C178.6 576 64 461.4 64 320zM320 336C359.8 336 392 303.8 392 264C392 224.2 359.8 192 320 192C280.2 192 248 224.2 248 264C248 303.8 280.2 336 320 336z"/></svg>
+          	👤
           </span>
           <input
             type="text"
             class="form-control"
             id="username"
-            placeholder="Seu nome de usuário"
+            placeholder="User123"
             aria-describedby="username-addon"
             required
           />
@@ -99,10 +110,7 @@
         <label for="password" class="form-label">Senha</label>
         <div class="input-group">
           <span class="input-group-text" id="password-addon">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="black" class="bi bi-lock" viewBox="0 0 16 16" aria-hidden="true">
-              <path d="M8 1a3 3 0 0 0-3 3v3h6V4a3 3 0 0 0-3-3z"/>
-              <path d="M5 8h6v5a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8z"/>
-            </svg>
+            🔒
           </span>
           <input
             type="password"
@@ -119,10 +127,7 @@
         <label for="confirm-password" class="form-label">Confirme a Senha</label>
         <div class="input-group">
           <span class="input-group-text" id="confirm-password-addon">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16" aria-hidden="true">
-              <path d="M8 1a3 3 0 0 0-3 3v3h6V4a3 3 0 0 0-3-3z"/>
-              <path d="M5 8h6v5a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8z"/>
-            </svg>
+            🔒
           </span>
           <input
             type="password"
@@ -135,7 +140,7 @@
         </div>
       </div>
 
-      <button type="submit" class="btn btn-login">LOGIN</button>
+      <button type="submit" class="btn btn-login">CADASTRO</button>
     </form>
   </main>
 </body>

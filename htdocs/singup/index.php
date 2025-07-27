0bfc27064341c8a8a2c4c41f72fed5459bdc7d4f
Thiago Sousa <thiagosousa81@gmail.com>
2025-07-27 00:55:16 +0000

    :sparkles: feat: § registro de usuário §

--- htdocs/singup/index.php
+++ htdocs/singup/index.php
@@ -1,3 +1,8 @@
+<?php
+require "../php/crud.php";
+$cls = new database();
+?>
+
 <!DOCTYPE html>
 <html lang"pt-br">
 <head>
@@ -13,7 +18,7 @@
   <main class="login-container">
     <h1 class="login-title text-center">Portuelho</h1>
 
-    <form>
+    <form method="POST">
       <div class="mb-3 position-relative">
         <label for="email" class="form-label">Email</label>
         <div class="input-group">
@@ -24,6 +29,7 @@
             type="email"
             class="form-control"
             id="email"
+            name="email"
             placeholder="email@exemplo.com"
             aria-describedby="email-addon"
             required
@@ -41,6 +47,7 @@
             type="text"
             class="form-control"
             id="name"
+            name="name"
             placeholder=""
             aria-describedby="name-addon"
             required
@@ -58,6 +65,7 @@
             type="text"
             class="form-control"
             id="username"
+            name="username"
             placeholder="User123"
             aria-describedby="username-addon"
             required
@@ -75,6 +83,7 @@
             type="password"
             class="form-control"
             id="password"
+            name="password"
             placeholder="●●●●●●●●●●●●●●●"
             aria-describedby="password-addon"
             required
@@ -92,6 +101,7 @@
             type="password"
             class="form-control"
             id="confirm-password"
+            name="confirm-password"
             placeholder="●●●●●●●●●●●●●●●"
             aria-describedby="confirm-password-addon"
             required
@@ -103,6 +113,106 @@
 
       <button type="submit" class="btn btn-login">CADASTRO</button>
     </form>
+
+    <?php
+      if (isset($_POST['email']) && isset($_POST['name']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['confirm-password'])) {
+          $nome = addslashes($_POST['name']);
+          $email = strtolower(addslashes($_POST['email']));
+          $username = str_replace('@', '', addslashes($_POST['username']));
+          $pwd = addslashes($_POST['password']);
+          $cpwd = addslashes($_POST['confirm-password']);                    
+
+          $mysqli = $cls->GetLinkMySQLI();
+
+          $sql = "SELECT EMAIL_ALUNO FROM ALUNO WHERE EMAIL_ALUNO = '" . base64_encode($email) . "'";
+          $result = $mysqli->query($sql);
+
+          if ($result->num_rows > 0) {
+              ?>
+              <div class="alert alert-danger alert-dismissible">
+                  <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                  <strong>Portuelho 🐰: </strong> Endereço de E-mail já cadastrado no sistema!
+              </div>
+              <?php
+          }
+          $sql = "SELECT ARROBA_ALUNO FROM ALUNO WHERE ARROBA_ALUNO = '" . base64_encode($username) . "'";
+          $result = $mysqli->query($sql);
+          if ($result->num_rows > 0) {
+              ?>
+              <div class="alert alert-danger alert-dismissible">
+                  <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                  <strong>Portuelho 🐰: </strong> Este @ de usuário já existe no sistema!
+              </div>
+              <?php
+          } else {
+              if (!empty($nome) && !empty($email) && !empty($username) && !empty($pwd) && !empty($cpwd)) {
+                  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                      ?>
+                      <div class="alert alert-danger alert-dismissible">
+                          <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                          <strong>Portuelho 🐰: </strong> Endereço de E-mail inválido!
+                      </div>
+                      <?php
+                  } else {
+                      if ($pwd == $cpwd) {
+
+                      //Definindo data de cadastro
+                      $timezone = new DateTimeZone('America/Sao_Paulo');
+                      $now = new DateTime('now', $timezone);
+                      $data_cadastro = $now->format('Y-m-d');
+
+                      // Valores padrão para campos opcionais
+                      $nivel = 1;
+                      $nivel_escrita = 1;
+                      $ofensiva = 0;
+                      $qi = 100;
+                      $vocabulario = 1;
+
+                      // Hash da senha
+                      $hash = password_hash($pwd, PASSWORD_ARGON2ID, [
+                          'memory_cost' => 1 << 17, // 128 MB
+                          'time_cost'   => 4,       // 4 iterações
+                          'threads'     => 2,       // 2 threads paralelas
+                      ]);
+                      
+
+                      $stmt = $mysqli->prepare("INSERT INTO `ALUNO` (`NOME_ALUNO`, `EMAIL_ALUNO`, `ARROBA_ALUNO`, `SENHA_ALUNO`, `DATA_CADASTRO`, `NIVEL`, `NIVEL_ESCRITA`, `OFENSIVA`, `QI`, `VOCABULARIO`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                      $stmt->bind_param("sssssiiii", base64_encode($nome), base64_encode($email), base64_encode($username), $hash, $data_cadastro, $nivel, $nivel_escrita, $ofensiva, $qi, $vocabulario);
+                      $stmt->execute();
+                      $stmt->close();
+                      $mysqli->close();
+
+
+                      ?>
+                      <!-- Mensagem de Sucesso -->
+                      <div class="alert alert-success alert-dismissible">
+                          <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                          <strong>Portuelho 🐰🥕: </strong> Usuário cadastrado com sucesso!
+                      </div>
+                      <?php
+
+
+                  } else {
+                      ?>
+                      <div class="alert alert-danger alert-dismissible">
+                          <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                          <strong>Portuelho 🐰: </strong> As senhas não conferem!
+                      </div>
+                      <?php
+                  }
+                  }
+              } else {
+                  ?>
+                  <div class="alert alert-danger alert-dismissible">
+                      <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                      <strong>Portuelho 🐰: </strong> Preencha todos os campos!
+                  </div>
+                  <?php
+              }
+          }
+
+      }
+    ?>
   </main>
 </body>
 </html>

0dfd49350ac7babeee473526c2c372f45a890e4e
Thiago Sousa <thiagosousa81@gmail.com>
2025-07-27 00:55:41 +0000

    :sparkles: feat: § Adicionando logo §

--- htdocs/profile.php
+++ htdocs/profile.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ANG3L Profile</title>
-    
+    <link rel="shortcut icon" href="/src/logo.webp">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     
